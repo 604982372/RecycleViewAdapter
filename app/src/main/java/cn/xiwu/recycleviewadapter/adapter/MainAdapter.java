7bbfaed22a16c1a65cef4c3380eb2e1fa75fb580
604982372 <604982372@users.noreply.github.com>
2017-12-25 13:54:26 +0800

    Update MainAdapter.java

--- app/src/main/java/cn/xiwu/recycleviewadapter/adapter/MainAdapter.java
+++ app/src/main/java/cn/xiwu/recycleviewadapter/adapter/MainAdapter.java
@@ -2,14 +2,14 @@ package cn.xiwu.recycleviewadapter.adapter;
 
 import android.content.Context;
 
-import cn.xiwu.recycleviewadapter.adapter.base.RecycleBaseAdapter;
+import cn.xiwu.recycleviewadapter.adapter.base.BaseRecycleAdapter;
 import cn.xiwu.recycleviewadapter.bean.Teacher;
 
 /**
- * Created by zuzu on 2017/11/30.
+ * Created by xiwu on 2017/11/30.
  */
 
-public class MainAdapter extends RecycleBaseAdapter<Teacher,TeacherHolder>
+public class MainAdapter extends BaseRecycleAdapter<Teacher,TeacherHolder>
 {
     public MainAdapter(Context context)
     {
